fd48b91a65b95cf3831abafa8d6a2ce8d9ac41d5
Igor Oliveira <igorbatista.ads@gmail.com>
2024-02-18 20:37:16 -0300

    Initial commit, for create menu and add images

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu ul");
+
+  menuToggle.addEventListener("click", function () {
+    menu.classList.toggle("open");
+  });
+});
