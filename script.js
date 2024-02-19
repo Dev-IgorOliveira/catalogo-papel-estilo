603a3cab4f31ba4e89ce28a8315dde519044e4cb
Igor Oliveira <igorbatista.ads@gmail.com>
2024-02-19 07:57:07 -0300

    Add new page for papers and initial grid for images

--- script.js
+++ script.js
@@ -6,3 +6,45 @@ document.addEventListener("DOMContentLoaded", function () {
     menu.classList.toggle("open");
   });
 });
+
+function expandPhoto(button) {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modal-img");
+  var photo = button.parentNode.querySelector(".photo-img");
+  modalImg.src = photo.src;
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+// Função para expandir a foto
+function expandPhoto(button) {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modal-img");
+  var photo = button.parentNode.querySelector(".photo-img");
+  modalImg.src = photo.src;
+  modal.style.display = "block";
+
+  // Adicionando event listener para fechar o modal ao pressionar a tecla ESC
+  window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeModal();
+    }
+  });
+
+  // Adicionando event listener para fechar o modal ao clicar em qualquer lugar da tela
+  modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+}
+
+// Função para fechar o modal
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
